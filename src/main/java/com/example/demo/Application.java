2da3aee2fa343df0d9bbf3fb2a6f5519b0dda7e4
aymanwaell <aymanwael@icloud.com>
2024-01-01 10:37:57 +0200

    JPA

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,32 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student maria = new Student(
+                    "Maria",
+                    "Jones",
+                    "mariajones@amigoscode.edu",
+                    21
+            );
+            Student maria2 = new Student(
+                    "Maria",
+                    "Jones",
+                    "mariajones2@amigoscode.edu",
+                    25
+            );
+            Student ahmed = new Student(
+                    "Ahmed",
+                    "Ali",
+                    "ahmedali@amigoscode.edu",
+                    18
+            );
+            System.out.println("adding maria and ahmed");
+            studentRepository.saveAll(List.of(maria,ahmed,maria2));
+            studentRepository.findStudentByEmail("ahmedali@amigoscode.edu")
+                    .ifPresentOrElse(System.out::println,() -> System.out.println("student with email ahmedali@amigoscode.edu not found"));
+            studentRepository.selectStudentsByFirstNameEqualsAndAgeIsGreaterThanEqualNative("Maria",21).forEach(System.out::println);
+        };
+    }
 }
