0fdfa4ec6e86b2dc88554dc5f3b2e123076ab76b
aymanwaell <aymanwael@icloud.com>
2024-01-12 11:44:11 +0200

    JPA

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -53,13 +53,36 @@ public class Application {
 
             StudentIdCard studentIdCard = new StudentIdCard
                     ("123456789", student);
-            studentIdCardRepository.save(studentIdCard);
+
+            student.setStudentIdCard(studentIdCard);
+
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L,1L),
+                    student,
+                    new Course("Tyazo Manakesho","Besho"),
+                    LocalDateTime.now()
+            ));
+            student.addEnrolment(new Enrolment(
+                    new EnrolmentId(1L,3L),
+                    student,
+                    new Course("Ahmed Maher","Besho"),
+                    LocalDateTime.now().minusDays(18)
+            ));
+            studentRepository.save(student);
             studentRepository.findById(1L)
-                            .ifPresent(System.out::println);
-            studentIdCardRepository.findById(1L)
+                            .ifPresent(s -> {
+                                System.out.println("fetch book lazy...");
+                                List<Book> books = student.getBooks();
+                                books.forEach(book -> {
+                                    System.out.println(s.getFirstName() +" borrowed "+book.getBookName());
+                                });
+                            });
+                                    };
+            /*
+                    studentIdCardRepository.findById(1L)
                     .ifPresent(System.out::println);
+                   */
         };
-    }
 
     private void generateRandomStudents(StudentRepository studentRepository) {
         Faker faker = new Faker();
